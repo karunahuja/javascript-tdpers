c85ddc51398b0e48a1e3b1f78f1a2d83bdcf79ba
karun <karunahuja1998@gmail.com>
2020-08-23 19:56:14 +0530

    Internal vs External js

---
+++ myscript.js
@@ -0,0 +1,22 @@
+
+
+//single and double quotes
+alert("Hello");
+alert('Hello');
+alert('He said "Hello"');
+
+//semicolons 
+prompt("are u sure?") //acceptable
+prompt("really?");   //preferred
+
+
+alert("Helo World");
+confirm("Are you sure to contunue");
+x=prompt("Enter your name");
+alert(x);
+function openSesame(){
+    window.open("http://www.esn.com","_blank","top=100,left=200,height=400,width=600,toolbar=no,resizable=no");
+}
+
+
+  

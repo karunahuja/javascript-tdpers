eca4ed6add8cf09c7044c0f39ede84592fbe64c2
karun <karunahuja1998@gmail.com>
2020-08-23 20:12:42 +0530

    Using javascript variables

--- myscript.js
+++ myscript.js
@@ -1,3 +1,23 @@
+var x;
+alert(x);
+
+var y=null;
+alert(y);
+
+var z=1.23432;
+alert(z);
+
+var a=Math.PI;
+alert(a);
+
+var b=["red","green","blue"];
+alert(b);
+
+
+var x={make:"Ford",model:"Focus",year:"2019"};
+alert(x.year);
+
+
 function myfunc1(){
 alert("button1");
 }
